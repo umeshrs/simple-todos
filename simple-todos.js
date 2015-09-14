b66d364247e6ee97e68ef1f376802c4472fdb50f
Umesh <rsu1991@gmail.com>
2015-09-14 14:02:34 +0530

    Added a form and a submit event handler to add new tasks. Sorted the task list in reverse chronological order.

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,27 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-    	return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    'submit .new-task': function () {
+      // Prevent default browser form submit
+      event.preventDefault();
+      console.log(event);
+
+      // Get the value from the form input element
+      var text = event.target.text.value;
+
+      // Insert new task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form input element
+      event.target.text.value = "";
     }
   });
 }
