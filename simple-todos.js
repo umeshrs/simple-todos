02840eba46b03fced02bc1490827f892236beb61
Umesh <rsu1991@gmail.com>
2015-11-25 14:13:41 +0530

    Added method setPrivate to set tasks to private.

--- simple-todos.js
+++ simple-todos.js
@@ -89,5 +89,17 @@ Meteor.methods({
     Tasks.update(taskId, {
       $set: {checked: checkedStatus}
     });
+  },
+  setPrivate: function (taskId) {
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, {
+      $set: {private: ! task.private}
+    });
   }
 });
